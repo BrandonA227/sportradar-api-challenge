654bfbd4b38503bd687c4b4a0b1c114ecce8cfed
Brandon Ames <Brandon.A.Ames@gmail.com>
2022-07-25 00:14:48 -0500

    Add ability to switch type of id

--- src/display.js
+++ src/display.js
@@ -1,9 +1,13 @@
 import NHL from "./nhl";
 
 (() => {
+  const typeTags = document.querySelectorAll(".type-selector > li");
+  const idLabel = document.querySelector(".id-container > label");
   const idInput = document.getElementById("id-input");
   const seasonYearInput = document.getElementById("season-year");
   const submitBtn = document.querySelector(".download-csv-btn");
+
+  let idType = "Player";
 
   const downloadCSV = async () => {
     console.log(
@@ -11,7 +15,17 @@ import NHL from "./nhl";
     );
   };
 
+  const changeIdTag = (e) => {
+    typeTags.forEach((element) => element.classList.remove("active"));
+    e.target.classList.add("active");
+    idLabel.textContent = e.target.textContent + " ID";
+    idType = e.target.textContent;
+  };
+
   const _init = (() => {
     submitBtn.addEventListener("click", downloadCSV);
+    typeTags.forEach((element) => {
+      element.addEventListener("click", changeIdTag);
+    });
   })();
 })();
