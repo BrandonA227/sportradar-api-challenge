3a3a36a78d16becadfd5c17e9128782608eff9c2
Brandon Ames <Brandon.A.Ames@gmail.com>
2022-07-28 18:47:57 -0500

    Fix season year input bug

--- src/display.js
+++ src/display.js
@@ -83,36 +83,36 @@ import { convertJsonToCsv } from "./utils";
     let filteredData = {};
     const typeOfData = idType.toLowerCase();
     if (typeOfData === "player") {
-      const playerData = data[0].people[0];
-      const playerStatsData = data[1].stats[0].splits[0].stat;
-      filteredData.id = playerData.id;
-      filteredData.fullName = playerData.fullName;
-      filteredData.currentTeam = playerData.currentTeam.name;
-      filteredData.currentAge = playerData.currentAge;
-      filteredData.primaryNumber = Number(playerData.primaryNumber);
-      filteredData.playerPosition = playerData.primaryPosition.name;
-      filteredData.isRookie = playerData.rookie;
-      filteredData.assists = playerStatsData.assists;
-      filteredData.goals = playerStatsData.goals;
-      filteredData.games = playerStatsData.games;
-      filteredData.hits = playerStatsData.hits;
-      filteredData.points = playerStatsData.points;
+      const playerData = data[0]?.people[0];
+      const playerStatsData = data[1]?.stats[0]?.splits[0]?.stat;
+      filteredData.id = playerData?.id;
+      filteredData.fullName = playerData?.fullName;
+      filteredData.currentTeam = playerData?.currentTeam?.name;
+      filteredData.currentAge = playerData?.currentAge;
+      filteredData.primaryNumber = Number(playerData?.primaryNumber);
+      filteredData.playerPosition = playerData?.primaryPosition.name;
+      filteredData.isRookie = playerData?.rookie;
+      filteredData.assists = playerStatsData?.assists;
+      filteredData.goals = playerStatsData?.goals;
+      filteredData.games = playerStatsData?.games;
+      filteredData.hits = playerStatsData?.hits;
+      filteredData.points = playerStatsData?.points;
     } else if (typeOfData === "team") {
-      const teamData = data[0].teams[0].teamStats[0].splits[0];
-      const firstGameData = data[1].dates[0];
-      filteredData.id = teamData.team.id;
-      filteredData.teamName = teamData.team.name;
-      filteredData.teamVenueName = data[0].teams[0].venue.name;
-      filteredData.gamesPlayed = teamData.stat.gamesPlayed;
-      filteredData.wins = teamData.stat.wins;
-      filteredData.losses = teamData.stat.losses;
-      filteredData.points = teamData.stat.pts;
-      filteredData.goalsPerGame = teamData.stat.goalsPerGame;
-      filteredData.firstGameDate = firstGameData.date;
+      const teamData = data[0]?.teams[0]?.teamStats[0]?.splits[0];
+      const firstGameData = data[1]?.dates[0];
+      filteredData.id = teamData?.team?.id;
+      filteredData.teamName = teamData?.team.name;
+      filteredData.teamVenueName = data[0]?.teams[0]?.venue?.name;
+      filteredData.gamesPlayed = teamData?.stat?.gamesPlayed;
+      filteredData.wins = teamData?.stat?.wins;
+      filteredData.losses = teamData?.stat?.losses;
+      filteredData.points = teamData?.stat?.pts;
+      filteredData.goalsPerGame = teamData?.stat?.goalsPerGame;
+      filteredData.firstGameDate = firstGameData?.date;
       filteredData.firstGameOpponetName =
-        firstGameData.games[0].teams.away.team.id !== idType
-          ? firstGameData.games[0].teams.away.team.name
-          : firstGameData.games[0].teams.home.team.name;
+        firstGameData?.games[0]?.teams?.away?.team?.id !== idType
+          ? firstGameData?.games[0]?.teams?.away?.team?.name
+          : firstGameData?.games[0]?.teams?.home?.team?.name;
     }
     return filteredData;
   };
